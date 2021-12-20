2019380f560b58656a8dfcdde3f2707accacf7a0
Rafael Cardeal Lodi <65565453+rafaelcardeallodi@users.noreply.github.com>
2021-12-20 19:48:09 -0300

    Listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import styles from './styles.module.scss';
 
 import { GetStaticProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
 
-export default function Posts(){
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps{
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps){
   return(
     <>
       <Head>
@@ -15,21 +27,13 @@ export default function Posts(){
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de dezembro de 2021</time>
-            <strong>Anim quis excepteur aliqua officia ut ad laboris</strong>
-            <p>Qui enim dolor ut aliqua et consectetur sunt quis do nisi Lorem incididunt. Ullamco adipisicing voluptate ut elit mollit anim aute dolore proident aliqua elit. Laborum est enim fugiat est eu minim ipsum et anim esse aliquip ipsum. Culpa elit deserunt anim culpa dolor deserunt nostrud dolor cillum commodo Lorem. Culpa cupidatat exercitation anim non commodo Lorem officia in.</p>
-          </a>
-          <a href="#">
-            <time>11 de dezembro de 2021</time>
-            <strong>Nostrud eu mollit exercitation amet anim duis esse.</strong>
-            <p>Nisi ullamco anim aliquip aliquip sunt culpa ea aliqua proident. Fugiat eiusmod esse laboris sint irure incididunt commodo fugiat sit in laborum sit. Culpa tempor ipsum et mollit. Dolor aute aliquip tempor quis. Laborum magna eiusmod culpa eu est mollit elit fugiat duis est aliqua. Elit consequat voluptate sunt proident dolore cupidatat amet eu exercitation dolore velit. In pariatur eiusmod commodo excepteur enim in ex exercitation ea deserunt duis aliquip aliqua sint.</p>
-          </a>
-          <a href="#">
-            <time>08 de dezembro de 2021</time>
-            <strong>Anim aliqua dolor ut sint aute quis veniam dolore.</strong>
-            <p>Occaecat id nulla aute nostrud sunt consequat officia laboris minim incididunt velit labore. Deserunt ad nulla irure fugiat mollit excepteur. Elit laborum anim eu nulla cupidatat fugiat. Veniam exercitation pariatur eu exercitation. Dolor duis excepteur quis nisi qui duis eiusmod do laboris mollit nisi aute. Quis labore velit eu ut dolor enim dolor ut pariatur.</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -46,9 +50,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
