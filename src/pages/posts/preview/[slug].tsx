4c278f66d87b07c4e079d8aca4bf13847babd18a
Rafael Cardeal Lodi <65565453+rafaelcardeallodi@users.noreply.github.com>
2021-12-20 23:08:36 -0300

    Gerando previews estáticos

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import { getSession, useSession } from "next-auth/client";
 import Head from "next/head";
 import Link from "next/link";
@@ -27,7 +27,7 @@ export default function PostPreview({ post }: PostPreviewProps){
     if(session?.activeSubscription){
       router.push(`/posts/${post.slug}`)
     }
-  }, [session])
+  }, [session]);
 
   return(
     <>
@@ -56,7 +56,7 @@ export default function PostPreview({ post }: PostPreviewProps){
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return{
     paths: [],
     fallback: 'blocking'
@@ -85,6 +85,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return{
     props: {
       post
-    }
+    },
+    redirect: 60 * 30, // 30 minutes
   }
 }
