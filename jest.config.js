3b667c2c643cf257acf13ca49d522d4902007e0a
Rafael Cardeal Lodi <65565453+rafaelcardeallodi@users.noreply.github.com>
2022-02-02 07:26:41 -0300

    Testando SignInButton

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
   },
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     "\\.(scss|css|sass)$": "identity-obj-proxy"
   }
